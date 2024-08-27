2cc52a0c542dd72f2afde48d53277c4b3b4409a0
hamed18 <hamedcnec@gmail.com>
2024-08-27 23:36:42 +0600

    Load services from database using server api

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -18,7 +18,7 @@ const SignUp = () => {
         createUser(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user)
+                console.log('created user', user)
             })
             .catch(error => console.log(error))
 
